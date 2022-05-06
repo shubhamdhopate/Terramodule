c4c550f7aff162b55c7e2fbfc36afb55d25dd683
shubham dhopate <dhopate.shubh@gmail.com>
2022-05-06 16:18:58 +0530

    push msg

---
+++ openvpn/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  backend "s3" {
+    bucket  = "management-bucket-code"
+    key     = "openvpn/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "shubham"
+  }
+}
+
+data "terraform_remote_state" "vpc" {
+  backend = "s3"
+  config = {
+    bucket  = "management-bucket-code"
+    key     = "key/terraform.tfstate"
+    region  = "ap-south-1"
+    profile = "shubham"
+  }
+}
+
+
+module "ec2-instance" {
+  source             = "OT-CLOUD-KIT/ec2-instance/aws"
+  version            = "0.0.3"
+  count_ec2_instance = var.count_ec2_instance
+  ec2_name           = var.ec2_name
+  public_ip          = var.public_ip
+  tags               = var.tags
+  volume_size        = var.volume_size
+  volume_type        = var.volume_type
+  ami_id             = var.ami_id
+  key_name           = var.key_name
+  instance_type      = var.instance_type
+  security_groups    = data.terraform_remote_state.vpc.outputs.web_sg_id
+  subnet             = flatten(data.terraform_remote_state.vpc.outputs.public_subnet_ids)[0]
+  encrypted_volume   = var.encrypted_volume
+}
+
---
+++ openvpn/var.tf
@@ -0,0 +1,54 @@
+variable "count_ec2_instance" {
+  description = "number of ec2 instance"
+  type        = number
+  default     = 1
+}
+
+variable "ec2_name" {
+  description = "Name of bastion"
+  type        = string
+  default     = "management-openvpn-server"
+}
+variable "public_ip" {
+  description = "Public Ip "
+  type        = bool
+  default     = true
+}
+
+variable "tags" {
+  description = "Additional tags"
+  type        = map(string)
+  default     = {}
+}
+variable "volume_size" {
+  description = "volume size"
+  type        = number
+  default     = 8
+}
+
+variable "volume_type" {
+  description = "volume type"
+  type        = string
+  default     = "gp2"
+}
+
+variable "ami_id" {
+  description = "Name of Launch configuration"
+  type        = string
+  default     = "ami-05ba3a39a75be1ec4"
+}
+variable "key_name" {
+  description = "Key name of Launch configuration"
+  type        = string
+  default     = "shubham_key"
+}
+variable "instance_type" {
+  description = "Name of Launch configuration"
+  type        = string
+  default     = "t2.micro"
+}
+variable "encrypted_volume" {
+  type        = bool
+  description = "Optional) Whether to enable volume encryption. Defaults to false. Must be configured to perform drift detection."
+  default     = false
+}
